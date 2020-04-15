b769f62a65aa9b64440869c2f2a34991528fadf9
Franciso Rafael de Castro Costa <rafael.costa@febracis.local>
2020-04-15 10:01:17 -0300

    resolução dos exercicios 06 e 07 cap 03

---
+++ 3-estrutura-condicional/exercicios-resolvidos/exerc-06/scripts.js
@@ -0,0 +1,14 @@
+/**
+ * 1. exerc 06
+ *
+ * Faça um programa quer receba um número inteiro e verifique
+ * se o número é par ou impar
+ */
+
+let num = prompt('Digite um número inteiro');
+
+if( num % 2 == 0 ) {
+    alert('È par');
+} else {
+    alert('È ímpar');
+}
---
+++ 3-estrutura-condicional/exercicios-resolvidos/exerc-07/scripts.js
@@ -0,0 +1,78 @@
+/**
+ * 1. exerc 07
+ *
+ * Faça um programa que receba quatro valores, I, A, B e C.
+ * I é um valor inteiro e positivo e A, B e C são valores reais.
+ * Escreva os números A, B e C obedecendo à tabela a seguir
+ *
+ *  Valor de I  | Forma de escrever
+ *      1       | A, B e C em ordem crescente
+ *      2       | A, B e C em ordem decrescente
+ *      3       | O maior fica entre os outros dois números
+ */
+
+let i = prompt('Digite um inteiro entre 1 e 3:');
+let a = prompt('Digite um número real:');
+let b = prompt('Digite mais um número real:');
+let c = prompt('Digite mais um número real:');
+
+i = parseInt(i);
+a = parseFloat(a);
+b = parseFloat(b);
+c = parseFloat(c);
+
+if( i === 1 ) {
+
+    if( a < b && a < c  ) {
+        if( b < c ) {
+            alert(`${a} - ${b} - ${c}`);
+        } else{
+            alert(`${a} - ${c} - ${b}`);
+        }
+    }
+
+    if( b < a && b < c  ) {
+        if( a < c ) {
+            alert(`${b} - ${a} - ${c}`);
+        } else{
+            alert(`${b} - ${c} - ${a}`);
+        }
+    }
+
+    if( c < a && c < b  ) {
+        if( b < a ) {
+            alert(`${c} - ${b} - ${a}`);
+        } else{
+            alert(`${c} - ${a} - ${b}`);
+        }
+    }
+
+} else if ( i === 2 ) {
+
+    if( a < b && a < c  ) {
+        if( b < c ) {
+            alert(`${c} - ${b} - ${a}`);
+        } else{
+            alert(`${b} - ${c} - ${a}`);
+        }
+    }
+
+    if( b < a && b < c  ) {
+        if( a < c ) {
+            alert(`${c} - ${a} - ${b}`);
+        } else{
+            alert(`${a} - ${c} - ${b}`);
+        }
+    }
+
+    if( c < a && c < b  ) {
+        if( b < a ) {
+            alert(`${a} - ${b} - ${c}`);
+        } else{
+            alert(`${b} - ${a} - ${c}`);
+        }
+    }
+
+} else if ( i === 3 ) {
+    console.log('È três');
+}
